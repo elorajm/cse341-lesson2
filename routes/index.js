63dbda26504ee7a114b6e0fe7c125eaa03e9a1d9
elorajm <eloraathias@gmail.com>
2026-01-21 19:33:13 -0600

    changes to lesson 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("CSE 341 Lesson 2 API is running");
+  res.send("CSE 341 Lesson 3 API is running");
 });
 
 router.use("/contacts", require("./contacts"));
